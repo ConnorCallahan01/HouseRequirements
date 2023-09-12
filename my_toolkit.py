75d1997a86636b1374433b2a3ae2faf824a06c34
Patrick Connor Callahan <62373361+ConnorCallahan01@users.noreply.github.com>
2023-09-11 18:21:22 -0700

    Update my_toolkit.py

--- my_toolkit.py
+++ my_toolkit.py
@@ -1,5 +1,9 @@
-from superagi.tools.base_tool import BaseToolkit, BaseTool
+from superagi.tools.base_tool import BaseToolkit, BaseTool, ToolConfiguration
 from typing import Type, List
+from superagi.tools.HouseRequirements.my_tool import HouseSearchTool
+from superagi.models.tool_config import ToolConfig
+from superagi.types.key_type import ToolConfigKeyType
+
 
 class HouseSearchToolkit(BaseToolkit):
     name: str = "House Search Toolkit"
@@ -8,5 +12,9 @@ class HouseSearchToolkit(BaseToolkit):
     def get_tools(self) -> List[BaseTool]:
         return [HouseSearchTool()]
     
-    def get_env_keys(self) -> List[str]:
-        return ['OPENAI_KEY', 'PROPGPT_API_KEY']
+    
+    def get_env_keys(self) -> List[ToolConfiguration]:
+        return [
+            ToolConfiguration(key='OPENAI_KEY', key_type=ToolConfigKeyType.STRING, is_required= True, is_secret = True),
+            ToolConfiguration(key='PROPGPT_API_KEY', key_type=ToolConfigKeyType.STRING, is_required= True, is_secret = True)
+        ]
